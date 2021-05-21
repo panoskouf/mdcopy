0cbdbb217af9f1af9ef4a9be887b0aabbd0dd8d9
panoskouf <panoskouf@outlook.com>
2021-05-21 23:08:12 +0200

    add custom format for youtube videos

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,8 @@
-type mode = 'url' | 'org-mode' | 'markdown';
+type mode = 'url' | 'org-mode' | 'markdown' | 'custom';
 
 interface state {
-  url: string;
+  host: string;
+  href: string;
   title: () => string;
 }
 
@@ -13,9 +14,9 @@ const __chrome_extension_mdcopy = (callback) => {
 __chrome_extension_mdcopy(() => {
   // get updated url and title bcz SPAs
   function state(): state {
-    const url = location.href;
+    const { host, href } = location;
     const title = () => document.title;
-    return { url, title };
+    return { href, host, title };
   }
 
   // some random css prefix
@@ -58,6 +59,54 @@ __chrome_extension_mdcopy(() => {
     }
   }
 
+  /* site specific custom functionality - start */
+  function isYouTubeSite(host) {
+    return host === 'www.youtube.com' || host === 'music.youtube.com';
+  }
+
+  function isYouTubeVideoLink(host, url) {
+    return isYouTubeSite(host) && url.search('watch') !== -1;
+  }
+
+  // prettier-ignore
+  const ytJunk = [
+    // check for end of line or space after each one
+    ' - YouTube', 'And Lyrics', 'and video', 'lyrics', 'lyric', 'video', 'clip', 'Audio', 'HD', 'Official', 'Unofficial', 'Edit', 'Follow Me', 'Music', 'Deep-House', 'House', 'rap', 'trap', 'pop', 'metal', 'Subtitulada Español', 'song', 'sold', 'free','Tradução', 'Beats', 'beat'
+  ]
+  const ytJunkRgx = new RegExp(ytJunk.join('|'), 'gi');
+
+  function getRandomEmojis(num: number) {
+    const emojis = [...'🎸🎵🎧🤘✨🎹🥁💖🎆🎇🧨🌄📻🍹😎😍🥳🎈🎉⚡🔥🎷🎶'];
+    let randomEmoji = () => emojis[Math.floor(Math.random() * emojis.length)];
+
+    let emojiStr = '';
+    for (let i = num; i--; ) emojiStr += randomEmoji();
+
+    return emojiStr;
+  }
+
+  function siteSpecificFormat(title) {
+    const { host, href } = state();
+
+    if (isYouTubeVideoLink(host, href)) {
+      // remove junk in song title
+      title = title
+        .replace(ytJunkRgx, '')
+        .replace('feat', 'ft')
+        .replace('remix', 'mix')
+        .replace(/[\{][\ ]*[\}]/g, '')
+        .replace(/[\(][\ ]*[\)]/g, '')
+        .trim('');
+      /* .replace(/([\ ]*)/g, ' ') */
+
+      title = `${getRandomEmojis(1)} ${title} ${getRandomEmojis(1)}`;
+      return { isCustom: true, newTitle: title };
+    } else {
+      return { isCustom: false, newTitle: title };
+    }
+  }
+  /* site specific custom functionality - end */
+
   // replace any brackets or parens that might break the format
   function formatTitle(title: string, mode: mode) {
     let newTitle = title;
@@ -83,18 +132,29 @@ __chrome_extension_mdcopy(() => {
     }
   }
 
-  function getLink(mode: mode, { url, title }: state) {
-    let formattedTitle = formatTitle(title(), mode);
+  function getLink(mode: mode, { href, title, host }: state) {
+    if (mode != 'custom') {
+      let formattedTitle = formatTitle(title(), mode);
 
-    let msgToUser = 'url copied';
-    if (mode === 'org-mode') {
-      url = `[[${url}][${formattedTitle}]]`;
-      msgToUser = 'org-link copied';
-    } else if (mode === 'markdown') {
-      url = `[${formattedTitle}](${url})`;
-      msgToUser = 'markdown-link copied';
+      let msgToUser = 'url copied';
+      let toClipboard = '';
+      if (mode === 'org-mode') {
+        toClipboard = `[[${href}][${formattedTitle}]]`;
+        msgToUser = 'org-link copied';
+      } else if (mode === 'markdown') {
+        toClipboard = `[${formattedTitle}](${href})`;
+        msgToUser = 'markdown-link copied';
+      }
+      copyToClipboard(toClipboard, msgToUser);
+    } else {
+      const { isCustom, newTitle } = siteSpecificFormat(title());
+      let msgToUser = !isCustom ? 'title & url copied' : '✨ copied ✨';
+
+      let url = !isYouTubeVideoLink(host, href) ? href : href.split('&')[0]; // exclude playlist
+      let toClipboard = newTitle + '\n\n' + url;
+
+      copyToClipboard(toClipboard, msgToUser);
     }
-    copyToClipboard(url, msgToUser);
   }
 
   /* jobs to run on keybindings */
